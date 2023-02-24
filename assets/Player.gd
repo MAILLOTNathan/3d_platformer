2c81f7e6a00d77f31073179a95cd60b64df3134f
ju-lit-hein <julien.ferdinand@epitech.eu>
2023-02-24 14:42:57 +0400

    player movement and camera but better

--- assets/Player.gd
+++ assets/Player.gd
@@ -7,14 +7,15 @@ export var min_angle = -80
 export var max_angle = 90
 
 export var speed = 10
-export var WALK_SPEED = 10
-export var RUN_SPEED = 40
+export var WALK_SPEED = 15
+export var RUN_SPEED = 25
 export var acceleration = 50
-export var friction = 20
+export var deceleration = 100
 
 export var gravity = 100
-export var jump_force = 35
+export var jump_force = 40
 export var MAX_JUMP = 2
+export var SPAWN = Vector3(0, 3.585, -4.04)
 
 var velocity = Vector3.ZERO
 var move_dir = Vector3.ZERO
@@ -27,7 +28,7 @@ func _ready():
 
 func _physics_process(delta):
 	cam.rotation_degrees.x = look_rot.x
-	if velocity == Vector3.ZERO:
+	if velocity != Vector3.ZERO:
 		rotation_degrees.y = look_rot.y
 	else:
 		cam.rotation_degrees.y = look_rot.y
@@ -35,7 +36,7 @@ func _physics_process(delta):
 	if (Input.is_action_just_pressed("Jump")):
 		if count_jump < MAX_JUMP:
 			count_jump += 1
-			velocity.y += jump_force
+			velocity.y = jump_force
 	if is_on_floor():
 		count_jump = 0
 	if not is_on_floor():
@@ -44,16 +45,19 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("Forward") or Input.is_action_pressed("Backward") or Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
 		if Input.is_action_pressed("Run"):
-			speed = WALK_SPEED
-		else:
 			speed = RUN_SPEED
-		move_dir.z = Input.get_action_strength("Forward") - Input.get_action_strength("Backward")
-		move_dir.x = Input.get_action_strength("Left") - Input.get_action_strength("Right")
-		move_dir.y = 0
-		velocity.x = lerp(velocity.x, move_dir.x * speed, acceleration * delta)
-		velocity.z = lerp(velocity.z, move_dir.z * speed, acceleration * delta)
-		
-		velocity = move_and_slide(velocity, Vector3.UP)
+		else:
+			speed = WALK_SPEED
+	move_dir = Vector3(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		0,
+		Input.get_action_strength("Backward") - Input.get_action_strength("Forward")
+	).normalized().rotated(Vector3.UP, rotation.y)
+	velocity.x = lerp(velocity.x, move_dir.x * speed, acceleration * delta)
+	velocity.z = lerp(velocity.z, move_dir.z * speed, acceleration * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+	if translation.y < -50:
+		translation = Vector3(0, 3.585, -4.04)
 		
 func _input(event):
 	if event is InputEventMouseMotion:
