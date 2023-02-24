171dca8e2b5dc589ce241e3df62eab8ac25f3383
MAILLOT Nathan <nathan.maillot@epitech.eu>
2023-02-24 16:38:57 +0400

    adding anim

--- assets/Turtle.gd
+++ assets/Turtle.gd
@@ -8,7 +8,6 @@ var velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	if trigger.is_colliding():
-		print("oui")
 		rotation_degrees.y = -rotation_degrees.y
 	else:
 		translation.z += speed * delta
