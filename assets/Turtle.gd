7215491cb9b3fb047acad6e262783284b1e4b705
MAILLOT Nathan <nathan.maillot@epitech.eu>
2023-02-24 15:45:40 +0400

    layer problem

--- assets/Turtle.gd
+++ assets/Turtle.gd
@@ -8,6 +8,7 @@ var velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	if trigger.is_colliding():
+		print("oui")
 		rotation_degrees.y = -rotation_degrees.y
 	else:
 		translation.z += speed * delta
