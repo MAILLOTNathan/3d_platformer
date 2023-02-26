bf298c9fb406a544b17e46bb0af037fcc13d80a9
ju-lit-hein <julien.ferdinand@epitech.eu>
2023-02-26 11:24:07 +0400

    world2 fini

--- Trigger.gd
+++ Trigger.gd
@@ -3,12 +3,13 @@ extends Spatial
 onready var error = $Error
 
 export var path = ""
+export(int) var coins_goal
 
 var stat = Stats
 
 func _on_Area_body_entered(body):
 	if body.get_name() == "Player":
-		if stat.coins == 3:
+		if stat.coins == coins_goal:
 			get_tree().change_scene(path)
 		else:
 			error.visible = true
